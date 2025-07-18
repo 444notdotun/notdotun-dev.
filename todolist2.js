9fb0295df1d844bbc69cbabde780d7ad5a44a6b1
notdotun <erik.edwards.jon@gmail.com>
2025-07-18 03:56:48 +0100

    notdotuncommit

--- todolist2.js
+++ todolist2.js
@@ -4,59 +4,84 @@ let ul = document.getElementById("todolist");
 let button = document.querySelector("button");
 let input = document.querySelector("input");
 let main = document.querySelector("main")
-
+let clear = document.getElementById("clear")
 let div = document.createElement("div")
-let pp =document.createElement("p");
+let pp = document.createElement("p");
 div.appendChild(pp)
-  
-let space = function() {
-    if(ul.children.length < 1){
+
+let space = function () {
+    if (ul.children.length < 1) {
         pp.textContent = "you have no to do for the day!"
-        pp.style.textDecoration = "wavy"
-    }else{
+        // bn();
+    } else {
         pp.textContent = ` you have ${ul.children.length} to do for the day`;
-        pp.style.textDecoration = "wavy"
+
     }
 }
+let clearbn = document.createElement("button")
 
 
-button.addEventListener("click", () =>{
+
+
+
+
+button.addEventListener("click", () => {
     let inputvalue = input.value;
-    
-    if(inputvalue ===""){ 
+
+    if (inputvalue === "") {
         alert("enter a to-do")
         return
     }
-     
-    
-    
+
+
+
     let li = document.createElement("li")
     li.textContent = inputvalue
-    li.style.textDecoration = "solid"
+    li.style.fontSize = "bold"
     li.style.textTransform = "capitalize"
     let completebn = document.createElement("button")
-    completebn.textContent = "complete?"
+    completebn.textContent = "complete"
     completebn.addEventListener("click", () => {
-        completebn.textContent  ="completed!"
+        completebn.textContent = "completed!"
         li.style.textDecoration = "line-through"
     })
     let deletebn = document.createElement("button")
     deletebn.textContent = "Delete"
-    
-    deletebn.addEventListener("click",() =>{
+
+    deletebn.addEventListener("click", () => {
         li.remove()
         space()
-        
+        bn()
     })
     input.value = "";
     input.focus();
-    
+
     li.appendChild(completebn)
     li.appendChild(deletebn)
     ul.appendChild(li)
-    
+
+    function bn() {
+
+        if (ul.children.length > 0) {
+            clearbn.textContent = "clear all"
+            clear.appendChild(clearbn)
+            clearbn.addEventListener("click", () => {
+                li.remove();
+                space()
+                if (ul.children.length === 0) {
+                    clear.removeChild(clearbn)
+                }
+            })
+        } else if (ul.children.length === 0) {
+            clear.removeChild(clearbn)
+        }
+
+
+    }
+
     space()
+    bn()
+
 })
 main.appendChild(div)
-
 space()
