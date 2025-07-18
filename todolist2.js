a24cc53bf30615654bea0f91a2d9f9f073cb05a3
notdotun <erik.edwards.jon@gmail.com>
2025-07-18 19:31:53 +0100

    notdotuncommmit

--- todolist2.js
+++ todolist2.js
@@ -1,5 +1,3 @@
-console.log("todolist");
-
 let ul = document.getElementById("todolist");
 let button = document.querySelector("button");
 let input = document.querySelector("input");
@@ -8,11 +6,10 @@ let clear = document.getElementById("clear")
 let div = document.createElement("div")
 let pp = document.createElement("p");
 div.appendChild(pp)
-
+    //space
 let space = function () {
     if (ul.children.length < 1) {
         pp.textContent = "you have no to do for the day!"
-        // bn();
     } else {
         pp.textContent = ` you have ${ul.children.length} to do for the day`;
 
@@ -29,35 +26,35 @@ button.addEventListener("click", () => {
         return
     }
 
-
+    //li
 
     let li = document.createElement("li")
     li.textContent = inputvalue
     li.style.fontSize = "bold"
     li.style.textTransform = "capitalize"
+    ///complete button
     let completebn = document.createElement("button")
     completebn.textContent = "complete"
     completebn.addEventListener("click", () => {
         completebn.textContent = "completed!"
         li.style.textDecoration = "line-through"
     })
+    ///delete buttom
     let deletebn = document.createElement("button")
     deletebn.textContent = "Delete"
-
     deletebn.addEventListener("click", () => {
         li.remove()
         space()
         bn()
     })
+    //appends
     input.value = "";
     input.focus();
-
     li.appendChild(completebn)
     li.appendChild(deletebn)
     ul.appendChild(li)
-
+    //clear function
     function bn() {
-
         if (ul.children.length > 0) {
             clearbn.textContent = "clear all"
             clear.appendChild(clearbn)
@@ -71,8 +68,6 @@ button.addEventListener("click", () => {
         } else if (ul.children.length === 0) {
             clear.removeChild(clearbn)
         }
-
-
     }
 
     space()
